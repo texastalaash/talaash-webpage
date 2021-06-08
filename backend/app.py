21e58091f363cc6bc4b3299c4e383db6def3dffc
Amitesh.yeleswarapu@utexas.edu <amitesh.yeleswarapu@utexas.edu>
2021-06-07 19:53:10 -0500

    connected to mongo atlas

--- backend/app.py
+++ backend/app.py
@@ -1,10 +1,19 @@
 from flask import Flask, request
 # cors is used to relax security, only for development
 from flask_cors import CORS
+import pymongo
+# Needed to verify the server is who it says it is. Mac won't work without and Cloud stuff might not either.
+import certifi
 
 # create a Flask app
 app = Flask(__name__)
 CORS(app)
+
+client = pymongo.MongoClient("mongodb+srv://user:pass@talaash.zp2uq.mongodb.net/usersdb?retryWrites=true&w=majority", tlsCAFile=certifi.where())
+Users_DB = client.get_database('user_information')
+Account_Info = Users_DB.users
+Account_Info.insert_one({"username":"name", "password":"pass"})
+
 
 # A simple home page to backend server
 @app.route('/')
